ff40718723db895574e078d03064abbff6f43545
dscottyoung2022 <131805518+dscottyoung2022@users.noreply.github.com>
2024-06-27 09:30:32 -0400

    API Data Information Formatted
    Formatted API for Humidity, wind speed, conditions, and date to be updated based on city entered in search bar.

--- index.js
+++ index.js
@@ -2,10 +2,40 @@ function updateWeatherData(response) {
   let temperatureElement = document.querySelector("#weather-app-temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionConditions = document.querySelector("#description");
+  let descriptionHumidity = document.querySelector("#humidity");
+  let windSpeed = document.querySelector("#wind-speed");
+  let dayTime = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
   cityElement.innerHTML = response.data.city;
+  dayTime.innerHTML = formatDate(date);
+  descriptionConditions.innerHTML = response.data.condition.description;
+  descriptionHumidity.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeed.innerHTML = `${response.data.wind.speed} mph`;
   temperatureElement.innerHTML = Math.round(temperature);
-  console.log(temperature);
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
